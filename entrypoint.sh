89775e0b6f1f13b2b0d7b57f9f7468704598bbfb
ameliakiara <amelia.kiaraa@gmail.com>
2022-09-27 13:55:38 +0700

    First Commit

---
+++ entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+cd /home/container
+
+# Make internal Docker IP address available to processes.
+export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
+
+# Replace startup variables
+MODIFIED_STARTUP=$(echo -e ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')
+echo ":/home/container$ ${MODIFIED_STARTUP}"
+
+# Run the server
+eval ${MODIFIED_STARTUP}
